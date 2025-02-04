4146b0bd66586951ad0afee83e7ead52f007f71e
pier <piervincenzo.salierno@gmail.com>
2025-02-04 20:04:41 +0100

    Spring Initializr

---
+++ immobile/src/test/java/it/immobiliare/immobile/ImmobileApplicationTests.java
@@ -0,0 +1,13 @@
+package it.immobiliare.immobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
