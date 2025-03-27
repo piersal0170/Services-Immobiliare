71f0676b182390f453d94677802c12e686fa06c8
pier <piervincenzo.salierno@gmail.com>
2025-03-27 15:22:38 +0100

    creazione repository

---
+++ immobile/src/main/java/it/immobiliare/immobile/repository/ImmobileRepository.java
@@ -0,0 +1,10 @@
+package it.immobiliare.immobile.repository;
+
+import it.immobiliare.immobile.entity.ImmobileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImmobileRepository extends JpaRepository<ImmobileEntity, Long> {
+
+}
