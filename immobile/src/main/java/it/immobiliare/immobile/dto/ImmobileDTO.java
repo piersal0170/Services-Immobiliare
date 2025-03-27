4bcaa381f1749fe4091ba2c0b322b2661e4241a0
pier <piervincenzo.salierno@gmail.com>
2025-03-27 16:46:12 +0100

    aggiornamento dto e creazione mapper

--- immobile/src/main/java/it/immobiliare/immobile/dto/ImmobileDTO.java
+++ immobile/src/main/java/it/immobiliare/immobile/dto/ImmobileDTO.java
@@ -1,7 +1,6 @@
 package it.immobiliare.immobile.dto;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,7 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+
 public class ImmobileDTO {
     private String indirizzo;
     private int prezzo;
@@ -28,6 +27,10 @@ public class ImmobileDTO {
     private boolean posto_auto;
     private boolean accesso_disabili;
     private String descrizione;
+    private int visualizzazioni;
+    private boolean scuole;
+    private boolean parchi;
+    private boolean trasporti;
 
     //costruttore necessario per la ricerca generica degli immobili da parte di un utente
     public ImmobileDTO(int piano, int n_stanze, String categoria, char classe_energetica, boolean ascensore, boolean portineria, boolean posto_auto,
@@ -47,4 +50,61 @@ public class ImmobileDTO {
         this.riscaldamento = riscaldamento;
     }
 
+    //costruttore necessario per l'inserimento di un immobile da parte di un utente
+    public ImmobileDTO(String indirizzo, int prezzo, int piano, int dimensione, int n_stanze, String categoria, char classe_energetica,
+                       boolean ascensore, boolean portineria, boolean balcone, boolean terrazzo, boolean giardino, boolean climatizzazione, boolean riscaldamento,
+                       boolean posto_auto, boolean accesso_disabili, String descrizione){
+        this.indirizzo = indirizzo;
+        this.prezzo = prezzo;
+        this.piano = piano;
+        this.dimensione = dimensione;
+        this.n_stanze = n_stanze;
+        this.categoria = categoria;
+        this.classe_energetica = classe_energetica;
+        this.ascensore = ascensore;
+        this.portineria = portineria;
+        this.balcone = balcone;
+        this.terrazzo = terrazzo;
+        this.giardino = giardino;
+        this.climatizzazione = climatizzazione;
+        this.riscaldamento = riscaldamento;
+        this.posto_auto = posto_auto;
+        this.accesso_disabili = accesso_disabili;
+        this.descrizione = descrizione;
+
+    }
+
+    //costruttore necessario per recupare le statistiche degli immobili da parte di uno specifico utente
+    public ImmobileDTO(String indirizzo, int prezzo, int visualizzazioni){
+        this.indirizzo = indirizzo;
+        this.prezzo = prezzo;
+        this.visualizzazioni = visualizzazioni;
+    }
+
+    //costruttore necessario per mostrare all'utente tutte le informazioni dell'immobile
+    public ImmobileDTO(String indirizzo, int prezzo, int piano, int dimensione, int n_stanze, String categoria, char classe_energetica,
+                       boolean ascensore, boolean portineria, boolean balcone, boolean terrazzo, boolean giardino, boolean climatizzazione, boolean riscaldamento,
+                       boolean posto_auto, boolean accesso_disabili, String descrizione, boolean scuole, boolean parchi, boolean trasporti){
+        this.indirizzo = indirizzo;
+        this.prezzo = prezzo;
+        this.piano = piano;
+        this.dimensione = dimensione;
+        this.n_stanze = n_stanze;
+        this.categoria = categoria;
+        this.classe_energetica = classe_energetica;
+        this.ascensore = ascensore;
+        this.portineria = portineria;
+        this.balcone = balcone;
+        this.terrazzo = terrazzo;
+        this.giardino = giardino;
+        this.climatizzazione = climatizzazione;
+        this.riscaldamento = riscaldamento;
+        this.posto_auto = posto_auto;
+        this.accesso_disabili = accesso_disabili;
+        this.descrizione = descrizione;
+        this.scuole = scuole;
+        this.parchi = parchi;
+        this.trasporti = trasporti;
+
+    }
 }
---
+++ immobile/src/main/java/it/immobiliare/immobile/mapper/ImmobileMapper.java
@@ -0,0 +1,67 @@
+package it.immobiliare.immobile.mapper;
+
+import it.immobiliare.immobile.dto.ImmobileDTO;
+import it.immobiliare.immobile.entity.ImmobileEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImmobileMapper {
+    public ImmobileEntity toEntity(ImmobileDTO immobileDTO) {
+        ImmobileEntity immobileEntity = new ImmobileEntity();
+        immobileEntity.setIndirizzo(immobileDTO.getIndirizzo());
+        immobileEntity.setPrezzo(immobileDTO.getPrezzo());
+        immobileEntity.setPiano(immobileDTO.getPiano());
+        immobileEntity.setDimensione(immobileDTO.getDimensione());
+        immobileEntity.setN_stanze(immobileDTO.getN_stanze());
+        immobileEntity.setCategoria(immobileDTO.getCategoria());
+        immobileEntity.setClasse_energetica(immobileDTO.getClasse_energetica());
+        immobileEntity.setAscensore(immobileDTO.isAscensore());
+        immobileEntity.setPortineria(immobileDTO.isPortineria());
+        immobileEntity.setBalcone(immobileDTO.isBalcone());
+        immobileEntity.setTerrazzo(immobileDTO.isTerrazzo());
+        immobileEntity.setGiardino(immobileDTO.isGiardino());
+        immobileEntity.setClimatizzazione(immobileDTO.isClimatizzazione());
+        immobileEntity.setRiscaldamento(immobileDTO.isRiscaldamento());
+        immobileEntity.setPosto_auto(immobileDTO.isPosto_auto());
+        immobileEntity.setAccesso_disabili(immobileDTO.isAccesso_disabili());
+        immobileEntity.setDescrizione(immobileDTO.getDescrizione());
+
+        return immobileEntity;
+    }
+
+    public ImmobileDTO toDto(ImmobileEntity immobileEntity) {
+        ImmobileDTO immobileDTO = new ImmobileDTO();
+        immobileDTO.setIndirizzo(immobileEntity.getIndirizzo());
+        immobileDTO.setPrezzo(immobileEntity.getPrezzo());
+        immobileDTO.setPiano(immobileEntity.getPiano());
+        immobileDTO.setDimensione(immobileEntity.getDimensione());
+        immobileDTO.setN_stanze(immobileEntity.getN_stanze());
+        immobileDTO.setCategoria(immobileEntity.getCategoria());
+        immobileDTO.setClasse_energetica(immobileEntity.getClasse_energetica());
+        immobileDTO.setAscensore(immobileEntity.isAscensore());
+        immobileDTO.setPortineria(immobileEntity.isPortineria());
+        immobileDTO.setBalcone(immobileEntity.isBalcone());
+        immobileDTO.setTerrazzo(immobileEntity.isTerrazzo());
+        immobileDTO.setGiardino(immobileEntity.isGiardino());
+        immobileDTO.setClimatizzazione(immobileEntity.isClimatizzazione());
+        immobileDTO.setRiscaldamento(immobileEntity.isRiscaldamento());
+        immobileDTO.setPosto_auto(immobileEntity.isPosto_auto());
+        immobileDTO.setAccesso_disabili(immobileEntity.isAccesso_disabili());
+        immobileDTO.setDescrizione(immobileEntity.getDescrizione());
+        immobileDTO.setScuole(immobileEntity.isScuole());
+        immobileDTO.setParchi(immobileEntity.isParchi());
+        immobileDTO.setTrasporti(immobileEntity.isTrasporti());
+
+        return immobileDTO;
+    }
+
+    public ImmobileDTO toDto_VisualizzaStatistiche(ImmobileEntity immobileEntity) {
+        ImmobileDTO immobileDTO = new ImmobileDTO();
+        immobileDTO.setIndirizzo(immobileEntity.getIndirizzo());
+        immobileDTO.setPrezzo(immobileEntity.getPrezzo());
+        immobileDTO.setVisualizzazioni(immobileEntity.getVisualizzazioni());
+
+        return immobileDTO;
+    }
+
+}
